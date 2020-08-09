c94d69472a9b558e593c4ac436efc754bc4dce53
AnabelSalomone <anabel.salomone@gmail.com>
2020-08-09 17:32:25 +0200

    add hints

--- guess_philosopher.py
+++ guess_philosopher.py
@@ -1,12 +1,25 @@
+import random
+
 secret_philosopher = "Immanuel Kant"
 guess = ""
-guess_count = "0"
-guess_limit = "3"
+guess_count = 0
+guess_limit = 3
 out_of_guesses = False
 
-while guess !=secret_philosopher and not(out_of_guesses):
+
+hints = [
+    "He was a philosopher from the Lumieres period ",
+    "He was from Prussia ",
+    "He wrote a lot of critics ",
+    "He wrote about categories "
+]
+
+
+while guess != secret_philosopher and not out_of_guesses:
     if guess_count < guess_limit:
-        guess = input("Guess the philosopher: ")
+        generate_id = random.randint(0, 3)
+        previous_id = generate_id
+        guess = input(hints[generate_id])
         guess_count += 1
     else:
         out_of_guesses = True
